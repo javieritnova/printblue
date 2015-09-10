b2673eb8d87ce317133ec05d18b675a511453d05
javieritnova <javier.vargas@it-nova.co>
2015-09-10 08:39:16 -0500

    Create PrintBlue.java

---
+++ PrintBlue.java
@@ -0,0 +1,17 @@
+package com.casamotor.printblue;
+
+import android.util.Log;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class PrintBlue extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.e("**********************************************************************************************", "**********************************************************************************************");
+        return true;
+    }
+}
