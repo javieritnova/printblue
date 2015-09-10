1063d14b7dba858b6ce69d87b4210a1e19c76c43
javieritnova <javier.vargas@it-nova.co>
2015-09-10 09:26:40 -0500

    Create printblue.js

---
+++ www/printblue.js
@@ -0,0 +1,13 @@
+var printblue = {
+    createEvent: function(text, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'PrintBlue', // mapped to our native Java class called "Calendar"
+            'print', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "text": text
+            }]
+        ); 
+     }
+}
